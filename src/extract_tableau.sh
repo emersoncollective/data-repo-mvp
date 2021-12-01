9e65920187eb08a72b083530e76cb00306bd1c8a
Michael Dowd <mdgis@users.noreply.github.com>
2021-11-30 17:15:35 -0800

    execute both scripts setup cron

--- src/extract_tableau.sh
+++ src/extract_tableau.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# echo "Processing Dashboard 1"
-# python src/cbp_tableau_scraping.py \
-#      --url https://publicstats.cbp.gov/t/PublicFacing/views/CBPSBOEnforcementActionsDashboardsOCTFY22/SBOEncounters10935 \
-#      --data_element "SBO Table" \
-#      --skip_filters '["Fiscal Year"]' \
-#      --label 'cbp_encounters_dashboard_1'
+echo "Processing Dashboard 1"
+python src/cbp_tableau_scraping.py \
+     --url https://publicstats.cbp.gov/t/PublicFacing/views/CBPSBOEnforcementActionsDashboardsOCTFY22/SBOEncounters10935 \
+     --data_element "SBO Table" \
+     --skip_filters '["Fiscal Year"]' \
+     --label 'cbp_encounters_dashboard_1'
 
 echo "Processing Dashboard 2"
 python src/cbp_tableau_scraping.py \
