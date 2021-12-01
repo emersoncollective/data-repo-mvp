dd7d2c224bcf7f40e12daedd36dd55a97ee3b213
Michael Dowd <mdgis@users.noreply.github.com>
2021-11-30 16:29:01 -0800

    update the extract tableau.sh

--- src/extract_tableau.sh
+++ src/extract_tableau.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Processing Dashboard 1"
-python cbp_tableau_scraping.py \
+python .src/cbp_tableau_scraping.py \
      --url https://publicstats.cbp.gov/t/PublicFacing/views/CBPSBOEnforcementActionsDashboardsOCTFY22/SBOEncounters10935 \
      --data_element "SBO Table" \
      --skip_filters '["Fiscal Year"]' \
      --label 'cbp_encounters_dashboard_1'
 
 echo "Processing Dashboard 2"
-python cbp_tableau_scraping.py \
+python .src/cbp_tableau_scraping.py \
     --url https://publicstats.cbp.gov/t/PublicFacing/views/CBPSBOEnforcementActionsDashboardsOCTFY22/SBObyMonthDemo10935 \
     --data_element "Demo FYTD by Month (2)" \
     --skip_filters '["Demographic"]' \
