8c1f6b099cc9416f68a0119ca00dc35cf32c0a3f
Michael Dowd <mdgis@users.noreply.github.com>
2022-03-16 11:00:38 -0700

    update tableau dashboard urls

--- src/extract_tableau.sh
+++ src/extract_tableau.sh
@@ -2,14 +2,14 @@
 
 echo "Processing Dashboard 1"
 python src/cbp_tableau_scraping.py \
-     --url https://publicstats.cbp.gov/t/PublicFacing/views/CBPSBOEnforcementActionsDashboardsOCTFY22/SBOEncounters10935 \
+     --url https://publicstats.cbp.gov/t/PublicFacing/views/CBPSBOEnforcementActionsDashboardsFEBFY22/SBOEncounters2808\
      --data_element "SBO Table" \
      --skip_filters '["Fiscal Year"]' \
      --label 'cbp_encounters_dashboard_1'
 
 echo "Processing Dashboard 2"
 python src/cbp_tableau_scraping.py \
-    --url https://publicstats.cbp.gov/t/PublicFacing/views/CBPSBOEnforcementActionsDashboardsOCTFY22/SBObyMonthDemo10935 \
+    --url https://publicstats.cbp.gov/t/PublicFacing/views/CBPSBOEnforcementActionsDashboardsFEBFY22/SBObyMonthDemo2808\
     --data_element "Demo FYTD by Month (2)" \
     --skip_filters '["Demographic"]' \
     --label 'cbp_encounters_dashboard_2'
